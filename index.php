d7ea41cb4a10437081d2b9c6ca68ba3960d41f4c
Arko Elsenaar <a.elsenaar@situatiewin.nl>
2022-07-18 21:10:35 +0200

    tailwindcss added

--- index.php
+++ index.php
@@ -29,6 +29,12 @@
                         id="bordered-radio-2" type="radio" value="PHP.net" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                     <label for="bordered-radio-2" class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">PHP.net</label>
                 </div>
+                <div class="flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700 flex-1">
+                    <input 
+                        _="on click set #lmstdfyForm's @docs to 'tailwindcss'"
+                        id="bordered-radio-3" type="radio" value="TailwindCSS" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    <label for="bordered-radio-3" class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">TailwindCSS</label>
+                </div>
             </div>
             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
             <div class="relative">
